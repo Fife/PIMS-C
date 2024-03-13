a56958752bf64a87c1a55aa01faa6d33e516fa2e
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2024-03-12 18:25:41 -0600

    Update PIMS_C_UART_EXAMPLE.c

--- src/PIMS_E Interface/PIMS_E Side/PIMS_C_UART_EXAMPLE.c
+++ src/PIMS_E Interface/PIMS_E Side/PIMS_C_UART_EXAMPLE.c
@@ -12,10 +12,9 @@ Spring 2024
 //Specific UART function that transmits a byte
 int main(){
     // PIMS-E Constucts the data structure and uses the provided transmit function.
-    
+
     SensorReport sr1 = {"12/20/2024 10:27:30", 32, 2.2, 22, 2.1, -1, 1, -2, 2, -3, 3, 0.1, 0.2, 0.3, 0.25, 0.6};
     transmitSensorReport(sr1);
-
 
     return 1;
 }
